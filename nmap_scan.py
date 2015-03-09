9f477cb2f0f3aafd64bc958b38e4acb958f57664
mbernhardt6 <mbernhardt6@gmail.com>
2015-03-08 21:53:16 -0700

    Tweak
    Tweak and line length

--- nmap_scan.py
+++ nmap_scan.py
@@ -3,15 +3,16 @@ import sys
 
 def nmapScan(subnet, output_type, output_file):
   dev_null = open('/dev/null', 'w')
-  sys.stdout = dev_null
   stylesheet = "/home/<user>/netscan/nmap.xsl"
   if output_type == "xml":
     out_switch = "-oX"
   else:
     out_switch = "-oN"
-  ExitCode = subprocess.call("nmap -O %s %s --stylesheet %s %s" % (out_switch, output_file, stylesheet, subnet), shell=True)
+  ExitCode = subprocess.call("nmap -O %s %s --stylesheet %s %s" % (out_switch,
+      output_file, stylesheet, subnet), shell=True, stdout=dev_null)
   return ExitCode
 
 def generateReport(xml_file, html_file):
-  ExitCode = subprocess.call("xsltproc %s -o %s" % (xml_file, html_file), shell=True)
+  ExitCode = subprocess.call("xsltproc %s -o %s" % (xml_file, html_file),
+      shell=True)
   return ExitCode
